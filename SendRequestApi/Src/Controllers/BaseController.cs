f2b439579a8d14e03636781ffa76aee74cd32675
d.antipov <7991ghost@gmail.com>
2023-10-29 14:21:22 +0300

    Add controller classes to make requests

---
+++ SendRequestApi/Src/Controllers/BaseController.cs
@@ -0,0 +1,7 @@
+﻿namespace SendRequestApi.Controllers;
+
+public abstract class BaseController
+{
+    private const string BaseUrl = "https://send-request.me/api";
+    protected readonly RestClient Client = new (new RestClientOptions(BaseUrl));
+}
---
+++ SendRequestApi/Src/Controllers/CompaniesController.cs
@@ -0,0 +1,23 @@
+﻿using SendRequestApi.ResponseModels;
+
+namespace SendRequestApi.Controllers;
+
+public class CompaniesController : BaseController
+{
+    private const string Url = "/companies";
+
+    public RestResponse<CompaniesModel> GetCompanies(string? status = null, int? limit = null, int? offset = null)
+    {
+        var request = new RestRequest(Url);
+        if (status is not null) request.AddParameter("status", status);
+        if (limit is not null) request.AddParameter("limit", (int)limit);
+        if (offset is not null) request.AddParameter("offset", (int)offset);
+        
+        return Client.ExecuteGet<CompaniesModel>(request);
+    }
+
+    public RestResponse<CompanyModel> GetCompanyById()
+    {
+        throw new NotImplementedException();
+    }
+}

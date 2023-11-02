7a1dd587f4d3032c52230ab8370bbdd6261f1c34
d.antipov <7991ghost@gmail.com>
2023-11-02 16:10:24 +0300

    Add schema assert to validate server responses against schemas

---
+++ SendRequestApi/Src/Asserts/SchemaAssert.cs
@@ -0,0 +1,20 @@
+﻿using NJsonSchema;
+using SendRequestApi.Enums;
+using SendRequestApi.Tests;
+
+namespace SendRequestApi.Asserts;
+
+public abstract class SchemaAssert
+{
+    public static void Matches(string? responseContent, JsonSchemaType schema, 
+        string errorMessage = "Schema validation ended with errors")
+    {
+        if (responseContent is null) 
+            throw new ArgumentNullException(nameof(responseContent), "Response content is null");
+        
+        var errors = JsonSchema.FromJsonAsync(TestsSetUp.Schemas?[schema.ToString()])
+            .Result.Validate(responseContent, SchemaType.OpenApi3);
+        
+        Assert.That(errors, Is.Empty, errorMessage);
+    }
+}
---
+++ SendRequestApi/Src/Enums/JsonSchemaType.cs
@@ -0,0 +1,19 @@
+﻿namespace SendRequestApi.Enums;
+
+public enum JsonSchemaType
+{
+    BaseUser,
+    Company,
+    CompanyDescription,
+    CompanyList,
+    CompanyStatus,
+    HttpValidationError,
+    MeResponse,
+    Meta,
+    ResponseUser,
+    TokenResponse,
+    TranslationLangs,
+    UserCredentials,
+    UsersList,
+    ValidationError
+}
